39e21395b2d52919cd31bc81559b38d027b2d08a
Tanapol Nearunchorn <tanapoln@wongnai.com>
2017-11-24 15:06:34 +0700

    support raw function

--- src/index.js
+++ src/index.js
@@ -1,41 +1,54 @@
 import checkValidOptions from './options'
 
+function functionBodyPushAttributes(t, path, options, componentName) {
+  let openingElement = null
+  const functionBody = path.get('body').get('body')
+  if (functionBody.parent && functionBody.parent.type === 'JSXElement') {
+    const jsxElement = functionBody.find((c) => {
+      return c.type === 'JSXElement'
+    })
+    if (!jsxElement) return
+    openingElement = jsxElement.get('openingElement')
+  } else {
+    const returnStatement = functionBody.find((c) => {
+      return c.type === 'ReturnStatement'
+    })
+    if (!returnStatement) return
+
+    const arg = returnStatement.get('argument')
+    if (!arg.isJSXElement()) return
+
+    openingElement = arg.get('openingElement')
+  }
+
+  if (!openingElement) return
+  openingElement.node.attributes.push(
+    t.jSXAttribute(
+      t.jSXIdentifier(options.attribute),
+      t.stringLiteral(options.format(componentName))
+    )
+  )
+}
+
 export default function ({types: t}) {
   return {
     visitor: {
+      FunctionDeclaration (path, state) {
+        // if (!path.parent.id || !path.parent.id.name) return;
+        if (!path.node.id || !path.node.id.name) return;
+
+        const options = checkValidOptions(state)
+        const componentName = path.node.id.name
+
+        functionBodyPushAttributes(t, path, options, componentName)
+      },
       ArrowFunctionExpression (path, state) {
         if (!path.parent.id || !path.parent.id.name) return;
 
         const options = checkValidOptions(state)
         const componentName = path.parent.id.name
 
-        let openingElement = null
-        const functionBody = path.get('body').get('body')
-        if (functionBody.parent && functionBody.parent.type === 'JSXElement') {
-          const jsxElement = functionBody.find((c) => {
-            return c.type === 'JSXElement'
-          })
-          if (!jsxElement) return
-          openingElement = jsxElement.get('openingElement')
-        } else {
-          const returnStatement = functionBody.find((c) => {
-            return c.type === 'ReturnStatement'
-          })
-          if (!returnStatement) return
-
-          const arg = returnStatement.get('argument')
-          if (!arg.isJSXElement()) return
-
-          openingElement = arg.get('openingElement')
-        }
-
-        if (!openingElement) return
-        openingElement.node.attributes.push(
-          t.jSXAttribute(
-            t.jSXIdentifier(options.attribute),
-            t.stringLiteral(options.format(componentName))
-          )
-        )
+        functionBodyPushAttributes(t, path, options, componentName)
       },
       ClassDeclaration (path, state) {
         let name = path.get('id')
